657495edd8952c929ff735a3d89c5b3ec20dc3ac
Ryan Collins <github@ryancollins.org>
2013-01-27 17:34:03 -0500

    removed class stuff from the manifest

--- manifests/localbrowser.pp
+++ manifests/localbrowser.pp
@@ -1,6 +1,4 @@
 # /etc/gbb/manifests/localbrowser.pp
-
-class localbrowser {
     user { "browser":
         ensure     	=> present,
         shell      	=> '/bin/bash',
@@ -92,4 +90,3 @@ class localbrowser {
         mode            => 755,
         source          => "/etc/gbb/files/localbrowserxsessions",
     }
-}
