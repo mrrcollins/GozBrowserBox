1465fe461d9eaf9b61a9fd59bcdba0e6cd3331f2
Ryan Collins <github@ryancollins.org>
2013-01-27 21:16:45 -0500

    Change profile directory, set to remove it automatically when user finished

--- files/localbrowserxsessions/run_browser.sh
+++ files/localbrowserxsessions/run_browser.sh
@@ -7,11 +7,9 @@ if test -z "$DBUS_SESSION_BUS_ADDRESS" ; then
         eval 'dbus-launch --sh-syntax --exit-with-session'
 fi
 
-dbus-launch /usr/bin/google-chrome --disable-bundled-ppapi-flash  
-#dbus-launch /usr/bin/google-chrome --user-data-dir="/home/browser/profiles/${PROFILE}" --no-default-browser-check chrome://signin/?source=0&next_page=http%3A%2F%2Fwww.kentoncityschools.org%2F
-#dbus-launch /usr/bin/google-chrome --user-data-dir="/home/browser/profiles/${PROFILE}" --no-first-run --no-default-browser-check --disable-file-system
+rm -Rf /home/`whoami`/profile
 
-#dbus-launch /usr/bin/firefox
+dbus-launch /usr/bin/google-chrome --user-data-dir="/home/`whoami`/profile/"
 
 kill `ps | grep dbus-launch | grep -v grep | awk '{print $1}'`
-
+rm -Rf /home/`whoami`/profile
